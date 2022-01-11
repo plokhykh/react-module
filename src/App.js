90dfb1cc0c8c82ffde9fcb6c63769013c90d20c2
plokhykh <mplokhykh@gmail.com>
2022-01-11 20:40:29 +0200

    hw1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Users from "./components/Users";
+import Posts from "./components/Posts";
+import Comments from "./components/Comments";
+
 
 function App() {
-  return (
-    <div >
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="box">
+                <span className='users'><Users/></span>
+                <span className='posts'><Posts/></span>
+            </div>
+            <div className="comments">
+                <Comments/>
+            </div>
+        </div>
+    );
 }
 
 export default App;
