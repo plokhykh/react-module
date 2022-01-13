d44c4a1c3c9cfa33c8d547194611348a88ed9891
plokhykh <mplokhykh@gmail.com>
2022-01-13 17:51:01 +0200

    cw-1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import {useState} from "react";
+
+import {postsService} from "./services/posts.service";
+import Users from "./components/users/Users";
+import PostsList from "./components/PostsList";
 import './App.css';
 
+
 function App() {
-  return (
-    <div >
-    </div>
-  );
+    const [posts, setPosts] = useState([]);
+
+    const getUserPosts = (userId) => {
+        postsService.getByUser(userId)
+            .then(response => setPosts(response))
+    }
+
+    return (
+        <div>
+            <Users getUserPosts={getUserPosts}/>
+            {posts && posts.map(post => <PostsList title={post.title} body={post.body}/>)}
+        </div>
+    );
 }
 
 export default App;
