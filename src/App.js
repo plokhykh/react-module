f92c523210c8eda1ba071b5d72c4ca25be2d31e5
plokhykh <mplokhykh@gmail.com>
2022-01-25 20:13:46 +0200

    additional_Rick&Morty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import {Route, Routes, Navigate} from "react-router-dom";
+
+import Layout from "./pages/Layout";
+import {EpisodesPage} from "./pages/episode-page/EpisodesPage";
+import {EpisodeDetails} from "./components/episode-details/EpisodeDetails";
+import {LocationsPage} from "./pages/LocationsPage";
+
 
 function App() {
-  return (
-    <div >
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path={'/'} element={<Layout/>}>
+                <Route path={''} element={<Navigate to={'episodes'}/>}/>
+
+                <Route path={'episodes'} element={<EpisodesPage/>}/>
+                <Route path={'/episodes/:episodeId'} element={<EpisodeDetails/>}/>
+
+                <Route path={'locations'} element={<LocationsPage/>}/>
+
+            </Route>
+
+
+
+
+        </Routes>
+
+    );
 }
 
 export default App;
