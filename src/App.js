b562cf73a15530ebbe809248ad8601ba438ac87f
plokhykh <mplokhykh@gmail.com>
2022-01-30 20:49:51 +0200

    lesson5-hw

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import CarsPage from "./pages/CarsPage";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./pages/Layout";
+import {UsersPage} from "./pages/UsersPage";
+import {PostsPage} from "./pages/PostsPage";
+import {CommentsPage} from "./pages/CommentsPage";
 
 function App() {
-  return (
-    <div >
-    </div>
-  );
+    return (
+
+        <Routes>
+            <Route path={'/'} element={<Layout/>}>
+                <Route path={'cars'} element={<CarsPage/>}/>
+                <Route path={'users'} element={<UsersPage/>}/>
+                <Route path={'posts'} element={<PostsPage/>}/>
+                <Route path={'comments'} element={<CommentsPage/>}/>
+            </Route>
+
+        </Routes>
+
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+
+import store from "./store/redux";
+import App from "./App";
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 
