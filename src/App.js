35705bf885b10ec35e638195bd51813cf7cca14f
plokhykh <mplokhykh@gmail.com>
2022-02-13 00:13:46 +0200

    movies-project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import {Route, Routes, Navigate} from 'react-router-dom';
+
+import {Layout, MoviesByGenrePage,MoviesPage} from './pages';
+import {MovieDetailsCard} from './components';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div >
-    </div>
+    <Routes>
+        <Route path={'/'} element={<Layout/>}>
+            <Route path={''} element={<Navigate to={'movies'}/>}/>
+            <Route path={'movies'} element={<MoviesPage/>}/>
+            <Route path={'movies/:id'} element={<MovieDetailsCard/>}/>
+            <Route path={'movies/genres/:genreId'} element={<MoviesByGenrePage/>}/>
+        </Route>
+    </Routes>
   );
 }
 
